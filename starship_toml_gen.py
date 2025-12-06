84fe874136350e18d3f704e3b3753d0175247fb9
YknotTYD <ethan.bertin-prevot@gmail.com>
2025-12-06 14:05:47 +0100

    [main] Improved starship_toml_gen.py - 'pass'.

--- starship_toml_gen.py
+++ starship_toml_gen.py
@@ -1,31 +1,60 @@
 #!/usr/bin/env python3
 #starship_toml_gen.py
 
+import sys
+from typing import TextIO, Tuple
+
 """
 Made to be used by install_starship_config.sh.
 """
 
-battery_display_text: str = """
+Color = Tuple[int, int, int]
+GRADIENT_SCALING: float = 0.85
+GRADIENT: Tuple[Color, Color] = (
+    (255, 255, 0), #gradient base  color (1%   battery)
+    (0,   255, 0), #gradient final color (100% battery)
+)
+
+BATTERY_DISPLAY: str = """
 [[battery.display]]
 threshold = {}
 style = "bold {}"
 """
 
-with open("starship.toml", "r") as file:
-    file_content = file.read().split("\n")
+def output_starship_toml(filepath: str = "starship.toml", out: TextIO = sys.stdout) -> None:
 
-replacement_index = file_content.index("#PYTHONANCHOR_REPLACE")
-file_content[replacement_index] = ""
+    """
+    **`output_starship_toml`**
 
-for i in range(0, 100):
+    **Args**:
 
-    color = (int(255 - (i / 100 * 255)), 255, 0)       #(255, 255, 0) to (0, 255, 0)
-    color = [int(i * 0.85) for i in color]             #reduce all channel values by 15%
-    color = [hex(i).removeprefix("0x") for i in color] #e.g. (255, 11, 0)       to ('ff', 'b', '0')
-    color = [f"{i:0>2}" for i in color]                #e.g. ('ff', 'b', '0')   to ('ff', '0b', '00')
-    color = "#" + "".join(color)                       #e.g. ('ff', '0b', '00') to '#ff0b00'
+        `filepath (str, optional)`:
+            The starship configuration file's path.
+            Defaults to 'starship.toml'.
 
-    file_content[replacement_index] += battery_display_text.format(i + 1, color)
+        `out (TextIO, optional)`:
+            The stream to ouput to.
+            Defaults to sys.stdout.
+    """
 
-for line in file_content:
-    print(line)
+    with open(filepath, "r") as file:
+        file_content = file.read().split("\n")
+
+    replacement_index = file_content.index("#PYTHONANCHOR_REPLACE")
+    file_content[replacement_index] = ""
+
+    for i in range(0, 100):
+
+        color = [(f * (i / 100) + b * (1 - i / 100)) for b, f in zip(*GRADIENT)] #(255, 255, 0) to (0, 255, 0)
+        color = [int(i * GRADIENT_SCALING) for i in color]                       #reduce all channel values by 15%
+        color = [hex(i).removeprefix("0x") for i in color]                       #e.g. (255, 11, 0)       to ('ff', 'b', '0')
+        color = [f"{i:0>2}" for i in color]                                      #e.g. ('ff', 'b', '0')   to ('ff', '0b', '00')
+        color = "#" + "".join(color)                                             #e.g. ('ff', '0b', '00') to '#ff0b00'
+
+        file_content[replacement_index] += BATTERY_DISPLAY.format(i + 1, color)
+
+    for line in file_content:
+        out.write(line)
+
+if __name__ == '__main__':
+    output_starship_toml()
